03944f4dc930df57c8b7e47c209676004681e4cc
Jenya Bykov <mothersasg@yandex.ru>
2020-01-07 23:19:22 +0500

    Init commit

---
+++ app/src/main/java/com/orange_infinity/gratitude/view/BaseActivity.kt
@@ -0,0 +1,17 @@
+package com.orange_infinity.gratitude.view
+
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+    }
+}
